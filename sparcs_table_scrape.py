5443dd1603195492cfa6189d5ebc70158d30ea60
venkashank <27744439+venkashank@users.noreply.github.com>
2023-04-09 00:15:06 +0200

    Added Year as a column

--- sparcs_table_scrape.py
+++ sparcs_table_scrape.py
@@ -2,6 +2,7 @@ import camelot
 from pathlib import Path
 from tqdm import tqdm
 import numpy as np
+import pandas as pd
 
 
 pdf_list = Path("pdfs/").glob("*.pdf")
@@ -10,6 +11,7 @@ pdf_list = Path("pdfs/").glob("*.pdf")
 def process_pdf(pdf_filename: str, folder: str):
     csv_name = pdf_filename.split("/")[-1].rstrip(".pdf")
     pfi = csv_name.split("_")[-1]
+    file_year = pdf_filename.split("_")[0].split("/")[-1][1:]
     tables = camelot.read_pdf(pdf_filename)
     table_counter = 1
     for table in tables:
@@ -33,6 +35,8 @@ def process_pdf(pdf_filename: str, folder: str):
 
         df["PFI"] = pfi
 
+        df["ADUIT_YEAR"] = file_year
+
         df.to_csv(f"{folder}/{csv_name}{table_counter}.csv", index=False)
         table_counter += 1
 
@@ -40,3 +44,9 @@ def process_pdf(pdf_filename: str, folder: str):
 if __name__ == "__main__":
     for pdf in tqdm(pdf_list):
         process_pdf(str(pdf), "csvs")
+    df_list = []
+    for csv_file in Path("csvs/").glob("*.csv"):
+        df = pd.read_csv(csv_file, dtype=str)
+        df_list.append(df)
+    df = pd.concat(df_list)
+    df.to_csv("SPARCS_AUDIT_REPORT.csv", index=False)
